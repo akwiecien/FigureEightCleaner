00502a4eaf875616e289f150861d8d324a7b4726
andrzej.kwiecien <andrzej.kwiecien@here.com>
2020-02-02 21:25:42 +0100

    Add project files.

---
+++ FigureEightCleaner/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace FigureEightCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(238, 313);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "how many jobs";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 22);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "KILL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(344, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "START 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(49, 313);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "init id";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(324, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "-";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 49);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(915, 254);
+            this.webBrowser1.TabIndex = 9;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(12, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(873, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(891, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(36, 20);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "go";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(450, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "START 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(513, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 379);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FigureEightCleaner/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace FigureEightCleaner
+{
+    public partial class Form1 : Form
+    {
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //webBrowser1.Navigate(new Uri("https://make.figure-eight.com/jobs"));
+            webBrowser1.ScriptErrorsSuppressed = true;
+            Navigate("https://make.figure-eight.com/jobs");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sleep(5, "");
+            LeftMouseClick(1210, 148);
+            Sleep(2, "");
+            //int initid = Convert.ToInt32(textBox1.Text);
+
+            int i = 1;
+            int max = Convert.ToInt32(textBox2.Text);
+
+            var lines = File.ReadAllLines("ids.txt").ToList();
+            var newList = File.ReadAllLines("ids.txt").ToList();
+
+            if (max > lines.Count)
+            {
+                max = lines.Count;
+            }
+            
+            foreach (string line in lines)
+            {
+                this.Text = $"{i} z {max}";
+                Application.DoEvents();
+
+                Clipboard.SetText(line.Trim());
+                LeftMouseClick(1800, 166);
+                Sleep(1, "");
+                LeftMouseClick(1795, 281);
+                Sleep(5, "space");
+                LeftMouseClick(619, 288);
+
+                SendKeys.Send("^v");
+                Sleep(1, "");
+                SendKeys.Send("{ENTER}");
+                Sleep(5, "id");
+
+                Application.DoEvents();
+                if (!textBox3.Text.Contains($"query={line.Trim()}"))
+                {
+                    File.Delete("ids.txt");
+                    File.WriteAllLines("ids.txt", newList);
+                    Environment.Exit(1);
+                }
+                Application.DoEvents();
+                LeftMouseClick(1849, 390);
+                Sleep(1, "");
+                LeftMouseClick(1762, 505);
+                Sleep(1, "");
+                LeftMouseClick(1186, 625);
+                Sleep(5, "finishing");
+
+                newList = newList.Where(x => x != line).ToList();
+
+                File.Delete("ids.txt");
+                File.WriteAllLines("ids.txt", newList);
+
+                if (i == max)
+                    break;
+
+                i++;
+            }
+        }
+
+        private void Sleep (int seconds, string what)
+        {
+            var max = seconds * 1000;
+            for (int i = 0; i <= max; i = i+10)
+            {
+                if (i%1000 == 0)
+                {
+                    
+                    label3.Text = $"{(i / 1000).ToString()} / {seconds} - {what}";
+                    Application.DoEvents();
+                }
+                Thread.Sleep(10);
+                Application.DoEvents();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Navigate(textBox3.Text);
+            }
+        }
+
+        private void Navigate(String address)
+        {
+            if (String.IsNullOrEmpty(address)) return;
+            if (address.Equals("about:blank")) return;
+            if (!address.StartsWith("http://") &&
+                !address.StartsWith("https://"))
+            {
+                address = "http://" + address;
+            }
+            try
+            {
+                webBrowser1.Navigate(new Uri(address));
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            textBox3.Text = webBrowser1.Url.ToString();
+            Application.DoEvents();
+        }
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Navigate(textBox3.Text);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Sleep(2, "");
+            LeftMouseClick(1210, 148);
+            Sleep(1, "");
+            //int initid = Convert.ToInt32(textBox1.Text);
+
+            int i = 1;
+            int max = Convert.ToInt32(textBox2.Text);
+
+            var lines = File.ReadAllLines("ids.txt").ToList();
+            var newLIst = File.ReadAllLines("ids.txt").ToList();
+
+            if (max > lines.Count)
+            {
+                max = lines.Count;
+            }
+            foreach (string line in lines)
+            {
+                var start = DateTime.Now;
+
+                this.Text = $"{i} z {max}";
+                Application.DoEvents();
+
+                Clipboard.SetText(line.Trim());
+                LeftMouseClick(1800, 166);
+                Sleep(1, "");
+                LeftMouseClick(1795, 281);
+                while (textBox3.Text != "https://make.figure-eight.com/jobs?scope=8b47f4bd-1349-477b-a44a-778bf5eb3eaa&query=&state=all&project=all&fields[]=tags&fields[]=title&fields[]=id&fields[]=owner_email&fields[]=alias")
+                {
+                    Sleep(1, "");
+                }
+                Sleep(1, "space");
+                LeftMouseClick(619, 288);
+                Sleep(1, "");
+
+                SendKeys.Send("^v");
+                Sleep(1, "");
+                SendKeys.Send("{ENTER}");
+                while (textBox3.Text != $"https://make.figure-eight.com/jobs?scope=8b47f4bd-1349-477b-a44a-778bf5eb3eaa&query={line.Trim()}&state=all&project=all&fields[]=tags&fields[]=title&fields[]=id&fields[]=owner_email&fields[]=alias")
+                {
+                    Sleep(1, "");
+                }
+                Sleep(1, "id");
+
+                Application.DoEvents();
+                if (!textBox3.Text.Contains($"query={line.Trim()}"))
+                {
+                    File.Delete("ids.txt");
+                    File.WriteAllLines("ids.txt", newLIst);
+                    Environment.Exit(1);
+                }
+                Application.DoEvents();
+                LeftMouseClick(1849, 390);
+                Sleep(1, "");
+                LeftMouseClick(1762, 505);
+                Sleep(1, "");
+                LeftMouseClick(1186, 625);
+                while (textBox3.Text != "https://make.figure-eight.com/jobs")
+                {
+                    Sleep(1, "");
+                }
+                Sleep(1, "finishing");
+
+                newLIst = newLIst.Where(x => x.Trim() != line.Trim()).ToList();
+                File.Delete("ids.txt");
+                File.WriteAllLines("ids.txt", newLIst);
+
+                var end = DateTime.Now;
+                var timeDiff = (end - start).TotalSeconds.ToString("#.#");
+                label4.Text = $"Time per job: {timeDiff}";
+                Application.DoEvents();
+
+                if (i == max)
+                {
+                    File.Delete("ids.txt");
+                    File.WriteAllLines("ids.txt", newLIst);
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
